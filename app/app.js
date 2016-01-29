4bc7fe3e5ee425e0905e99cfe057984a57fcda99
kaurav83 <torov440@gmail.com>
2016-01-29 14:28:27 +0200

    Update index.html and app.js

--- app/app.js
+++ app/app.js
@@ -1,5 +1,58 @@
-var swapApp = angular.module('swapApp', []);
+var scotchApp = angular.module('scotchApp', []);
 
-swapApp.controller('wrapController', function($scope){
-	$scope.message = 'Добро пожаловать на мою страницу';
+
+scotchApp.controller('mainController', function($scope) {
+
+    // сообщение, которое мы хотим отобразить
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+
+
+
+
+var scotchApp = angular.module('scotchApp', ['ngRoute']);
+
+// маршруты
+scotchApp.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+
+        
+        .when('/', {
+            templateUrl: 'home.html',
+            controller: 'mainController'
+        })
+
+       
+        .when('/about', {
+            templateUrl: '/about.html',
+            controller: 'aboutController'
+        })
+
+        
+        .when('/contact', {
+            templateUrl: 'contacts.html',
+            controller: 'contactController'
+        })
+        .when('/resume', {
+        	templateUrl: 'resume.html',
+        	controller: 'resumeController'
+        });
+        
+        $locationProvider.html5Mode(true);
+});
+
+scotchApp.controller('mainController', function($scope) {
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+scotchApp.controller('aboutController', function($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+scotchApp.controller('contactController', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});
+scotchApp.controller('resumeController', function($scope){
+	$scope.message = 'Read our works. We hope you like them.';
 });
